db71f9eb2117c40501c26a7a23189a9bb8a8afac
MichaelBoyo <103725264+MichaelBoyo@users.noreply.github.com>
2022-05-02 15:26:24 +0100

    updates

--- C4_Java/DecimalEquivalentOfBinary.java
+++ C4_Java/DecimalEquivalentOfBinary.java
@@ -9,39 +9,45 @@ public class DecimalEquivalentOfBinary {
         int num1= 0;
         int num2= 0;
         int num3= 0;
-        int num4= 0;
+        int num4 = 0;
+        int num5 = 0;
         int digits;
+        int num;
 
         int i=0;
-        System.out.print("Enter integer containing only 0 and 1: ");
-        int num=scanner.nextInt();
+        System.out.print("Enter a 5 digit integer containing only 0 and 1: ");
+        num=scanner.nextInt();
+
         int number = num;
 
         while (num!=0){
             num/=10;
             i++;
-            System.out.println(num);
         }
-        while(number!=0){
-            digits = number%10;
+
+        while(number!=0) {
+            digits = number % 10;
+            if (i == 5) {
+                num5 = digits;
+            }
             if (i == 4) {
-                num4 = digits;
+                num4 = digits * 2;
             }
             if (i == 3) {
-                num3 = digits *2;
+                num3 = digits * 4;
             }
             if (i == 2) {
-                num2 = digits *4;
+                num2 = digits * 8;
             }
             if (i == 1) {
-                num1 = digits*8;
+                num1 = digits*16;
             }
             number/=10;
 
             i--;
-            System.out.println(number);
+
         }
-        int total = num1 +num2 + num3+num4;
-        System.out.printf("the decimal equivalent of %d is %d",num,total);
+        int total = num1 +num2 + num3+num4+num5;
+        System.out.println("the decimal equivalent of  is "+total);
     }
 }
