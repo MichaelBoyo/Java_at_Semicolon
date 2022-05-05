293339d4246ed363b88e5cabc4960b44304d6d7e
MichaelBoyo <103725264+MichaelBoyo@users.noreply.github.com>
2022-05-05 16:15:12 +0100

    updates

--- C5_Java/ModifiedCompoundInterest.java
+++ C5_Java/ModifiedCompoundInterest.java
@@ -3,14 +3,21 @@ package C5_Java;
 public class ModifiedCompoundInterest {
     public static void main(String[] args) {
         double a;
+        int aa;
+        double dCent;
+        int cent;
         int p= 1000;
         double r =0.05;
         int n;
-        System.out.printf("%s%20s%n","Year","Amount");
+        System.out.printf("%s%20s%15s%n","Year","dollar Amount","Cents");
         do{
             for(n=1;n<=10;n++){
-                a= p* Math.pow(1+r,n);
-                System.out.printf("%4d%,22.2f%n",n,a);
+                a= ( p* Math.pow(1+r,n));
+                aa= (int)a;
+                dCent= (a*10) % 10;
+                cent = (int) (dCent*10);
+
+                System.out.printf("%4d%15d%20d%n",n,aa,cent);
 
             }
             System.out.println();
