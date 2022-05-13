71fbee1a6dc0889ce4004ee3c3e4079bf8f9b999
MichaelBoyo <103725264+MichaelBoyo@users.noreply.github.com>
2022-05-13 15:38:30 +0100

    updates

--- C6_Java/SecureRandomNum.java
+++ C6_Java/SecureRandomNum.java
@@ -8,7 +8,7 @@ import static Java_Flex.Nokia.display;
 public class SecureRandomNum {
     public static void main(String[] args) {
         SecureRandom number = new SecureRandom();
-        int random = number.nextInt();
+        int random = number.nextInt(2);
         display(String.valueOf(random));
 
     }
