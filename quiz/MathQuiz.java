49ac7a64edb7a6d6ae06820be5e0a1fb3bc64623
michael <boyomichaelbidemi@gmail.com>
2022-06-19 01:57:43 +0100

    okay

--- quiz/MathQuiz.java
+++ quiz/MathQuiz.java
@@ -5,9 +5,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MathQuiz extends Quiz {
-    static Scanner scanner = new Scanner(System.in);
-    protected final ArrayList<String> questions = new ArrayList<String>();
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
+    protected final ArrayList<String> questions = new ArrayList<>();
     private int difficulty = 1;
+    private final String[] operators = {"+", "*", "-", "/"};
+    private int numA;
+    private int numB;
+    private int correct;
 
     public int getDifficulty() {
         return difficulty;
@@ -18,48 +23,42 @@ public class MathQuiz extends Quiz {
     }
 
     public int setQuiz() {
-        int correct = 0;
-        Random random = new Random();
-        String[] operators = {"+", "*", "-", "/"};
-        String operator = "";
-        int num1 = 0;
-        int num2 = 0;
+        String operator;
         for (int i = difficulty; i < difficulty + 10; i++) {
-            num1 += (int) Math.pow(i, 2);
-            num2 += i;
+            numA += (int) Math.pow(i, 2);
+            numB += i;
             operator = operators[random.nextInt(operators.length)];
-            String question = String.format("%d %s %d = ", num1, operator, num2);
+            String question = String.format("%d %s %d = ", numA, operator, numB);
             questions.add(question);
-
 
             System.out.print(question);
             long answer = scanner.nextInt();
             switch (operator) {
                 case "+" -> {
-                    if (num1 + num2 == answer) {
+                    if (numA + numB == answer) {
                         correct++;
                         System.out.println("correct");
-                    } else System.out.printf("wrong, answer is %d%n", num1 + num2);
+                    } else System.out.printf("wrong, answer is %d%n", numA + numB);
                 }
                 case "-" -> {
-                    if (num1 - num2 == answer) {
+                    if (numA - numB == answer) {
                         correct++;
                         System.out.println("correct");
-                    } else System.out.printf("wrong, answer is %d%n", num1 - num2);
+                    } else System.out.printf("wrong, answer is %d%n", numA - numB);
 
                 }
                 case "/" -> {
-                    if (num1 / num2 == answer) {
+                    if (numA / numB == answer) {
                         correct++;
                         System.out.println("correct");
-                    } else System.out.printf("wrong, answer is %d%n", num1 / num2);
+                    } else System.out.printf("wrong, answer is %d%n", numA / numB);
 
                 }
                 case "*" -> {
-                    if ((long) num1 * num2 == answer) {
+                    if ((long) numA * numB == answer) {
                         correct++;
                         System.out.println("correct");
-                    } else System.out.printf("wrong, answer is %d%n", num1 * num2);
+                    } else System.out.printf("wrong, answer is %d%n", numA * numB);
 
                 }
             }
